268b46b14df24d43f116492bcd1c7a1da8a9d391
asamal <sasha.samal@gmail.com>
2020-11-08 18:12:24 +0300

    Formatting fixed

--- src/main/java/com/samal/greenstone/GreenstoneApplication.java
+++ src/main/java/com/samal/greenstone/GreenstoneApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GreenstoneApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GreenstoneApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GreenstoneApplication.class, args);
+    }
 }
