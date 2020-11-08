1f877cf3ea1fca336c2c540d20eada5eadfdbc0e
asamal <sasha.samal@gmail.com>
2020-11-08 16:47:41 +0300

    Spring Boot project created

---
+++ src/main/java/com/samal/greenstone/GreenstoneApplication.java
@@ -0,0 +1,13 @@
+package com.samal.greenstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samal/greenstone/GreenstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samal.greenstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
