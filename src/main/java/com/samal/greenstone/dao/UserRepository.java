12d21664d1c6f686d0542e3b2572f1aa7d5166bf
asamal <sasha.samal@gmail.com>
2020-11-10 22:39:31 +0300

    GS-16: Spring Data REST added

--- src/main/java/com/samal/greenstone/dao/UserRepository.java
+++ src/main/java/com/samal/greenstone/dao/UserRepository.java
@@ -2,11 +2,12 @@ package com.samal.greenstone.dao;
 
 import com.samal.greenstone.domain.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource
 public interface UserRepository extends CrudRepository<User, Long> {
-
     User findById(long id);
 
     List<User> findByName(String name);
