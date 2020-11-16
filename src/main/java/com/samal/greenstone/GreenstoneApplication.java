fb0428f93e3b6ed9ac31ff565a253ef34029d1cf
Aliaksandr Samal <sasha.samal@gmail.com>
2020-11-16 12:14:57 +0300

    GS-23 (#13)
    * GS-23: two providers
    
    * GS-23: Add OAuth2

--- src/main/java/com/samal/greenstone/GreenstoneApplication.java
+++ src/main/java/com/samal/greenstone/GreenstoneApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.Map;
 
@@ -18,6 +20,14 @@ public class GreenstoneApplication {
     @GetMapping("/user")
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
         return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+
+    @GetMapping("/error")
+    @ResponseBody
+    public String error(HttpServletRequest request) {
+        String message = (String) request.getSession().getAttribute("error.message");
+        request.getSession().removeAttribute("error.message");
+        return message;
     }
 
     public static void main(String[] args) {
