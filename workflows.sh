915491f452998641b41212c1b36db3e28feeaec9
asamal <sasha.samal@gmail.com>
2020-11-20 20:48:47 +0300

    Workflow-template

--- workflows.sh
+++ workflows.sh
@@ -1,13 +1,13 @@
 # read the workflow template
-WORKFLOW_TEMPLATE=$(cat .github/workflow-template.yaml)
+WORKFLOW_TEMPLATE=$(cat .github/workflow-template.yml)
 
 # iterate each route in routes directory
-for ROUTE in $(ls routes); do
-    echo "generating workflow for routes/${ROUTE}"
+for MODULE in $(ls modules); do
+    echo "generating workflow for routes/${MODULE}"
 
     # replace template route placeholder with route name
-    WORKFLOW=$(echo "${WORKFLOW_TEMPLATE}" | sed "s/{{ROUTE}}/${ROUTE}/g")
+    WORKFLOW=$(echo "${WORKFLOW_TEMPLATE}" | sed "s/{{MODULE}}/${MODULE}/g")
 
     # save workflow to .github/workflows/{ROUTE}
-    echo "${WORKFLOW}" > .github/workflows/${ROUTE}.yaml
+    echo "${WORKFLOW}" > .github/workflows/${MODULE}.yaml
 done
