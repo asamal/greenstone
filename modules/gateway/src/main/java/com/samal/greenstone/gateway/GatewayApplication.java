4d1db3aacd5803f04c1380906bb43f8ce57f2419
asamal <sasha.samal@gmail.com>
2020-11-21 00:50:14 +0300

    Fixed tests

--- modules/gateway/src/main/java/com/samal/greenstone/gateway/GatewayApplication.java
+++ modules/gateway/src/main/java/com/samal/greenstone/gateway/GatewayApplication.java
@@ -2,12 +2,19 @@ package com.samal.greenstone.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.codec.ServerCodecConfigurer;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public ServerCodecConfigurer serverCodecConfigurer() {
+		return ServerCodecConfigurer.create();
 	}
 
 }
