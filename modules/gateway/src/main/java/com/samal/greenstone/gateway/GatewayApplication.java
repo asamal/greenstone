448706b1a28c2a968a32d4088eaf50d57e097ee4
asamal <sasha.samal@gmail.com>
2020-11-23 16:21:34 +0300

    Jaeger fixed

--- modules/gateway/src/main/java/com/samal/greenstone/gateway/GatewayApplication.java
+++ modules/gateway/src/main/java/com/samal/greenstone/gateway/GatewayApplication.java
@@ -7,58 +7,54 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-import reactor.netty.http.client.HttpClient;
 
 @EnableConfigurationProperties(UriConfiguration.class)
 @SpringBootApplication
 @RestController
 public class GatewayApplication extends WebSecurityConfigurerAdapter {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 
-	@Bean
-	public ServerCodecConfigurer serverCodecConfigurer() {
-		return ServerCodecConfigurer.create();
-	}
+    @Bean
+    public ServerCodecConfigurer serverCodecConfigurer() {
+        return ServerCodecConfigurer.create();
+    }
 
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(p -> p
-						.path("/**")
-						.filters(f -> f.addRequestHeader("Hello", "World"))
-						.uri("http://localhost:8082"))
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/**")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://localhost:8082"))
 
-				.build();
-	}
+                .build();
+    }
 
-	@RequestMapping("/fallback")
-	public Mono<String> fallback() {
-		return Mono.just("fallback");
-	}
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("fallback");
+    }
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
-		// @formatter:off
-		http
-				.authorizeRequests(a -> a
-						.antMatchers("/**").permitAll()
-						.anyRequest().anonymous()
-				);
-		// @formatter:on
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
+        // @formatter:off
+        http
+                .authorizeRequests(a -> a
+                        .antMatchers("/**").permitAll()
+                        .anyRequest().anonymous()
+                );
+        // @formatter:on
+    }
 
 }
---
+++ start-modules.sh
@@ -0,0 +1,1 @@
+docker-compose -f docker/dc-local.yml up --build --force-recreate --no-deps
