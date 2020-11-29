e994fd87b412094bb221cb8c99f5f905f25fb7ab
asamal <sasha.samal@gmail.com>
2020-11-29 22:52:36 +0300

    WIP - [+] tracing A <-> B, [-] OAuth2

--- modules/gateway/src/main/java/com/samal/greenstone/gateway/GatewayApplication.java
+++ modules/gateway/src/main/java/com/samal/greenstone/gateway/GatewayApplication.java
@@ -7,23 +7,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.codec.ServerCodecConfigurer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import reactor.core.publisher.Mono;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Collections;
-import java.util.Map;
 
 @EnableConfigurationProperties(UriConfiguration.class)
 @SpringBootApplication
@@ -34,10 +20,10 @@ public class GatewayApplication /*extends WebSecurityConfigurerAdapter*/  {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-    @GetMapping("/user")
-    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-        return Collections.singletonMap("name", principal.getAttribute("name"));
-    }
+//    @GetMapping("/user")
+//    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+//        return Collections.singletonMap("name", principal.getAttribute("name"));
+//    }
 
 
 //    @Override
@@ -47,19 +33,19 @@ public class GatewayApplication /*extends WebSecurityConfigurerAdapter*/  {
 //                    .resourceChain(false);
 //            registry.setOrder(1);
 //    }
+//
+//    @GetMapping("/error")
+//    @ResponseBody
+//    public String error(HttpServletRequest request) {
+//        String message = (String) request.getSession().getAttribute("error.message");
+//        request.getSession().removeAttribute("error.message");
+//        return message;
+//    }
 
-    @GetMapping("/error")
-    @ResponseBody
-    public String error(HttpServletRequest request) {
-        String message = (String) request.getSession().getAttribute("error.message");
-        request.getSession().removeAttribute("error.message");
-        return message;
-    }
-
-    @Bean
-    public ServerCodecConfigurer serverCodecConfigurer() {
-        return ServerCodecConfigurer.create();
-    }
+//    @Bean
+//    public ServerCodecConfigurer serverCodecConfigurer() {
+//        return ServerCodecConfigurer.create();
+//    }
 
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
