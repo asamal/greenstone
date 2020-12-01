1cf7d1f8dc1a2c68dfc09eec42adc6c5a0f5000b
Aliaksandr Samal <sasha.samal@gmail.com>
2020-12-01 23:34:03 +0300

    Gateway + Jaeger + OAuth2 (#34)
    * WIP
    
    * WIP
    
    * WIP
    
    * gitignore
    
    * WIP
    
    * WIP - [+] tracing A <-> B, [-] OAuth2
    
    * Working! gateway + oauth2 + jaeger
    
    * Gateway updated

--- modules/gateway/src/main/java/com/samal/greenstone/gateway/GatewayApplication.java
+++ modules/gateway/src/main/java/com/samal/greenstone/gateway/GatewayApplication.java
@@ -7,36 +7,50 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.codec.ServerCodecConfigurer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.util.Collections;
+import java.util.Map;
+
+@EnableWebSecurity
 @EnableConfigurationProperties(UriConfiguration.class)
 @SpringBootApplication
 @RestController
-public class GatewayApplication extends WebSecurityConfigurerAdapter {
+public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-    @Bean
-    public ServerCodecConfigurer serverCodecConfigurer() {
-        return ServerCodecConfigurer.create();
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
     }
 
+
+    @GetMapping("/error")
+    public Mono<String> error(ServerHttpRequest request) {
+        return Mono.empty();
+    }
+
+
     @Bean
-    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+    public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
         return builder.routes()
                 .route(p -> p
-                        .path("/**")
+                        .path("/customers/**")
                         .filters(f -> f.addRequestHeader("Hello", "World"))
-                        .uri("http://localhost:8082"))
-
+                        .uri(uriConfiguration.getCoreUrl()))
                 .build();
     }
 
@@ -45,16 +59,14 @@ public class GatewayApplication extends WebSecurityConfigurerAdapter {
         return Mono.just("fallback");
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
-        // @formatter:off
-        http
-                .authorizeRequests(a -> a
-                        .antMatchers("/**").permitAll()
-                        .anyRequest().anonymous()
-                );
-        // @formatter:on
-    }
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
 
+        return http.authorizeExchange()
+                .pathMatchers("/", "/error", "/webjars/**").permitAll()
+                .pathMatchers("/customers", "/customers/**").permitAll()
+                .and().csrf().csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())
+                .and().logout()
+                .and().oauth2Login().and().build();
+    }
 }
--- modules/gateway/src/test/java/com/samal/greenstone/gateway/GatewayApplicationTests.java
+++ modules/gateway/src/test/java/com/samal/greenstone/gateway/GatewayApplicationTests.java
@@ -9,49 +9,29 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-		properties = {"coreUrl=http://localhost:${wiremock.server.port}"})
+        properties = {"coreUrl=http://localhost:${wiremock.server.port}"})
 @AutoConfigureWireMock(port = 0)
 class GatewayApplicationTests {
 
-	@Autowired
-	private WebTestClient webClient;
+    @Autowired
+    private WebTestClient webClient;
 
-	@Test
-	public void contextLoads() throws Exception {
-		//Stubs
-		stubFor(get(urlEqualTo("/get"))
-				.willReturn(aResponse()
-						.withBody("{\"headers\":{\"Hello\":\"World\"}}")
-						.withHeader("Content-Type", "application/json")));
-		stubFor(get(urlEqualTo("/delay/3"))
-				.willReturn(aResponse()
-						.withBody("no fallback")
-						.withFixedDelay(3000)));
+    @Test
+    public void contextLoads() throws Exception {
 
-				webClient
-				.get().uri("/get")
-				.exchange()
-				.expectStatus().isNotFound();
+        stubFor(get(urlEqualTo("/customers"))
+                .willReturn(aResponse()
+                        .withBody("{\"headers\":{\"Hello\":\"World\"}}")
+                        .withHeader("Content-Type", "application/json")));
 
-//		webClient
-//				.get().uri("/get")
-//				.exchange()
-//				.expectStatus().isOk()
-//				.expectBody()
-//				.jsonPath("$.headers.Hello").isEqualTo("World");
-
-//		webClient
-//				.get().uri("/delay/3")
-//				.header("Host", "www.hystrix.com")
-//				.exchange()
-//				.expectStatus().isOk()
-//				.expectBody()
-//				.consumeWith(
-//						response -> assertThat(response.getResponseBody()).isEqualTo("fallback".getBytes()));
-	}
-
+        webClient
+                .get().uri("/customers")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.headers.Hello").isEqualTo("World");
+    }
 }
