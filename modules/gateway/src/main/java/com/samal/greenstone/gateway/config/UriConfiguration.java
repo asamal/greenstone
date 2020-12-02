487e2214851d17853f75f00570fd0817bbbcca89
asamal <sasha.samal@gmail.com>
2020-12-02 23:19:58 +0300

    UriConfiguration updated

--- modules/gateway/src/main/java/com/samal/greenstone/gateway/config/UriConfiguration.java
+++ modules/gateway/src/main/java/com/samal/greenstone/gateway/config/UriConfiguration.java
@@ -1,11 +1,16 @@
 package com.samal.greenstone.gateway.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
 
 @ConfigurationProperties
+@Validated
 public class UriConfiguration {
 
-    private String coreUrl = "http://localhost:8082";
+    @NotEmpty
+    private String coreUrl;// = "http://localhost:8082";
 
     public String getCoreUrl() {
         return coreUrl;
