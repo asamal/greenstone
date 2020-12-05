e557ccc62d3093f66c700f7cb4d4fa9f88ec523f
asamal <sasha.samal@gmail.com>
2020-12-05 23:14:34 +0300

    Code clean up

--- modules/core/src/main/java/com/samal/greenstone/core/GreenstoneApplication.java
+++ modules/core/src/main/java/com/samal/greenstone/core/GreenstoneApplication.java
@@ -9,19 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class GreenstoneApplication {
-//    @GetMapping("/user")
-//    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-//        return Collections.singletonMap("name", principal.getAttribute("name"));
-//    }
-
-//    @GetMapping("/error")
-//    @ResponseBody
-//    public String error(HttpServletRequest request) {
-//        String message = (String) request.getSession().getAttribute("error.message");
-//        request.getSession().removeAttribute("error.message");
-//        return message;
-//    }
-
     public static void main(String[] args) {
         SpringApplication.run(GreenstoneApplication.class, args);
     }
