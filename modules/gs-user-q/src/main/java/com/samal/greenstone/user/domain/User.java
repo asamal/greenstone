72c04858e2d156f24ec8c6b00b9f3d2734c2fae1
asamal <sasha.samal@gmail.com>
2021-01-24 21:02:09 +0300

    Copy user logic to gs-user-q

---
+++ modules/gs-user-q/src/main/java/com/samal/greenstone/user/domain/User.java
@@ -0,0 +1,33 @@
+package com.samal.greenstone.user.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "gs_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private UUID uuid;
+    private String name;
+    private String email;
+
+    public User(String firstName, String lastName) {
+        this.name = firstName + " " + lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Use[id=%d, name='%s']",
+                id, name);
+    }
+}
