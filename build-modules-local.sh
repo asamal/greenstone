2060b7620aa13e0c0092bf4d5cf9360a48a59c66
asamal <sasha.samal@gmail.com>
2021-01-26 00:13:57 +0300

    Quarkus downgrade to java 11

--- build-modules-local.sh
+++ build-modules-local.sh
@@ -1,4 +1,5 @@
 mvn -f modules/gs-tree/pom.xml clean package
+mvn -f modules/gs-user-q/pom.xml clean package
 ./modules/gs-gateway/gradlew -p modules/gs-gateway clean build
 ./modules/gs-user/gradlew -p modules/gs-user clean build
 docker-compose -f docker/dc-local.yml build --no-cache
