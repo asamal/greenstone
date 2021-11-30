e4ca44d667a903df04572b8e3f4a90d2de6ae6a2
asamal <sasha.samal@gmail.com>
2021-11-30 21:00:02 +0300

    fix

--- modules/gs-user/src/main/java/com/samal/greenstone/user/domain/User.java
+++ modules/gs-user/src/main/java/com/samal/greenstone/user/domain/User.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 @Table(name = "gs_user")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private UUID uuid;
     private String name;
